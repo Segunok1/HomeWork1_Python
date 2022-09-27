5228e958bf3230e3b8834c47c3e1fa611a7c530c
sergeyGubarev <109827341+Segunok1@users.noreply.github.com>
2022-09-27 19:57:10 +0300

    Delete test.py

--- Python/test.py
+++
@@ -1,7 +0,0 @@
-def sum(a, b):
-    return a + b
- 
- 
-result = sum(4, 6)                  # result = 0
-print(f"sum(4, 6) = {result}")      # sum(4, 6) = 10
-print(f"sum(3, 5) = {sum(3, 5)}")   # sum(3, 5) = 8
