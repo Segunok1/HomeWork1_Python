e0fd43d9860c51aaa5791df715c9679462932bce
Segunok <k077ma777@yandex.ru>
2022-10-01 20:15:09 +0300

    Решение задачи №1

--- PythonTask3/task1.py
+++ PythonTask3/task1.py
@@ -4,9 +4,16 @@
 *Пример:*
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 """
-num = int(input('Введите число: '))
-list = []
-for i in range(num):
-    list.append(i)
+def sum_odd_position():
+    num = int(input('Введите число: '))
+    list = []
+    for i in range(num):
+        list.append(i)
     print(list)
-    
+    sum = 0
+    for i in range(len(list)):
+        if i % 2 != 0:
+            sum += i
+    print(sum)
+
+sum_odd_position()
