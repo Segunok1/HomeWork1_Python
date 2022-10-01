68dcc7e5d50701eff0a8219c1fed9abd784ccc5f
Segunok <k077ma777@yandex.ru>
2022-10-02 00:45:59 +0300

    Решение задачи №2

--- PythonTask3/task2.py
+++ PythonTask3/task2.py
@@ -5,18 +5,22 @@
 - [2, 3, 4, 5, 6] => [12, 15, 16];
 - [2, 3, 5, 6] => [12, 15]
 """
-
 from hashlib import new
 
+def the_product_of_pairs_of_numbers():
+    list1 = [2, 3, 4, 5, 6]
+    if len(list1) % 2 != 0:
+        print('Нечётное')
+        res = int(len(list1) / 2) + 1
+    else:
+        len(list) // 2 == 0
+        print('Чётное')
+        res = int(len(list1) / 2)
+    new_list = []
+    for i in range(0, res): 
+        new_list.append(list1[i] * list1[len(list1)-i-1])
+    return new_list
+   
+print(the_product_of_pairs_of_numbers())
 
-# list = [2, 3, 4, 5, 6]
-# new_list = []
-# if len(list) % 2 != 0:
-#     print(len(list))
-# else:
-#     len(list) // 2 == 0
-#     print(len(list))
-# for i in range(len(list)): 
-#     new_list = list[i] * list[len-i-1]
-#     new_list.appendK(new_list)
-# print(new_list)
+
