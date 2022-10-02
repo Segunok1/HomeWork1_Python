b90c17cf0d533fbcebadbd315ed68a802a70db9b
Segunok <k077ma777@yandex.ru>
2022-10-02 11:02:20 +0300

    Решение задачи №4

--- PythonTask3/task4.py
+++ PythonTask3/task4.py
@@ -13,12 +13,12 @@
 # num_conversion()
 
 
-# def num_conversion():
-#     n = int(input("Введите число: "))
-#     lst = []
-#     while n > 0:
-#         lst.append(str(n % 2))
-#         n //= 2
-#     return "".join(lst[::-1])
+def num_conversion():
+    n = int(input("Введите число: "))
+    lst = []
+    while n > 0:
+        lst.append(str(n % 2))
+        n //= 2
+    return "".join(lst[::-1])
 
-# print(num_conversion())
+print(num_conversion())
