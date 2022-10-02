60a307a2a4f8427ffb8b80bc2876056c5835f664
Segunok <k077ma777@yandex.ru>
2022-10-02 12:08:41 +0300

    Решение задачи №3

--- PythonTask3/task3.py
+++ PythonTask3/task3.py
@@ -6,4 +6,13 @@
 - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 """
 
+lst = [1.1, 1.2, 3.1, 5, 10.01]
+max = 0
+min = 1
+for i in lst:
+    if (i - int(i)) >= max:
+        max = i - int(i)
+    elif (i - int(i)) <= min:
+        min = i - int(i)
+print(max - min)   
 
