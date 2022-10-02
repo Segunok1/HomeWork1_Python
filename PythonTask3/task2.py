49c2a4c41c794c0ca78ce91cb9d46f1e48033a13
Segunok <k077ma777@yandex.ru>
2022-10-02 11:08:22 +0300

    Решение задачи №2

--- PythonTask3/task2.py
+++ PythonTask3/task2.py
@@ -8,14 +8,14 @@
 from hashlib import new
 
 def the_product_of_pairs_of_numbers():
-    lst = [2, 3, 5, 6]
+    lst = [2, 3, 4, 5, 6]
     if len(lst) % 2 != 0:
         res = int(len(lst) / 2) + 1
     else:
         res = int(len(lst) / 2)
     new_lst = []
     for i in range(0, res): 
-        new_lst.append(lst[i] * lst[len(lst)-i-1])
+        new_lst.append(lst[i] * lst[-i-1])
     return new_lst
    
 print(the_product_of_pairs_of_numbers())
