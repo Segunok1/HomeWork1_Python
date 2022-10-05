f8280b5ed581109bb501955b6fcaa2366ea2dac7
Segunok1 <k077ma777@yandex.ru>
2022-10-05 23:00:41 +0300

    Решение задачи №2

--- PythonTask4/task2.py
+++ PythonTask4/task2.py
@@ -1,17 +1,18 @@
-
-def factors(N):
-
-
-lst = []
-a = 2
-while a <= N:
-    if (N % a) == 0:
-        lst.append(a)
-        N /= a
-    else:
-        a += 1
-print(lst)
+"""
+Задайте последовательность чисел. Напишите программу, которая выведет список 
+неповторяющихся элементов исходной последовательности.
+"""
 
 
-N = int(input("Задайте натуральное число N: "))
-factors(N)
+def non_repeating_elements(nums):
+   lst = []
+   for num in nums:
+      if num in lst:
+         continue
+      else:
+         lst.append(num)
+   return lst
+
+
+nums = ["2", "2", 30, 54, "try", 30, "try"]
+print(non_repeating_elements(nums))
